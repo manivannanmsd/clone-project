54f0ea5f1de03ad00965ced6e52ec535bfb2007e
manivannanmsd <88765690+manivannanmsd@users.noreply.github.com>
2022-03-03 10:52:42 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exculsive app allows to send unlimited transactions without getting charged any fees.',
+    buttonLable: 'Get started',
+    img: require('../../images/svg1.png'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+    buttonLable: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg2.png'),
+    alt: 'Piggybank',
+    dark: true,
+    primary: true,
+    darkText: true
+};
+
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is extremely easy',
+    description: 'Get everything set up and ready in under 10 minutes. All you need to do is add your information and you`re ready to go. ',
+    buttonLable: 'Start Now',
+    img: require('../../images/svg3.png'),
+    alt: 'Papers',
+    dark: true,
+    primary: true,
+    darkText: true
+};
